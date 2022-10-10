db0005dfe231766e9c2c8ee9e253dfda9a9d4f8a
Tanya <veneficav@list.ru>
2022-10-10 22:09:15 +0700

    add draft

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int countElem = new Random().Next(3, 5);
+string[] array = new string[countElem];
+CreateArray();
+PrintArray(array);
+
+
+string[] CreateArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите строковое значение {i+1}");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+ void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+}
+
